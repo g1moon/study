940295daad97eff8556414b00d9c99e70ef889f3
g1moon <gmoon.cho@gmail.com>
2020-09-09 21:17:46 +0900

    [upload]

--- Algorithms-PS/coding_test_with_python/main.py
+++ Algorithms-PS/coding_test_with_python/main.py
@@ -1,60 +1,48 @@
-import copy
-#################
-#input-----------------------------------
-n = 5
-#input1
-# build_frame=[[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
-#input2
-build_frame=[[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]
-#---check--------------------------------------------------------
-def check(res):
-    for res_lst in res:
-        x, y, a = res_lst
-        #a가 0인경우 - 기둥
-        if a == 0:
-            if (y == 0) or [x,y,1] in res or [x-1,y,1] in res or [x,y-1,0] in res:
-                continue 
-            else:
-                print(res_lst)
-                return False 
-        #a가 1인경우 - 보
-        elif a == 1:
-            if [x+1,y-1, 0] in res or [x, y-1, 0] in res:
-                continue 
-            elif [x-1, y, 1] in res and [x+1, y, 1] in res:
-                continue 
-            else:
-                print(res_lst)
-                return False 
-    return True 
-    
-# res = [[0,0,0],[0,1,1],[1,1,1],[2,1,1],[3,1,1],[4,0,0]]
-# print(check(res))
-#-------------solution---------------------------------------
-def solution(n, build_frame):
-    import copy
+'''
+- (N*N)인 도시가 있다.
+- 각 칸은 (빈칸, 치킨집)
+- (r행, c열) , r과 c는 1부터시작
+- 치킨거리 : 집과 가장 가까운 치킨집사이의 거리 , 
+    -각각의 집은 치킨거리를 갖고있다
+- 도시의 치킨 거리 : 모든 집의 치킨거리 
 
-    res = []
-    
-    for info_lst in build_frame:
-        x, y, a, b = info_lst
-        tmp_res = copy.deepcopy(res)
-        #설치
-        if b == 1:
-            tmp_res.append([x,y,a]) #설치
-            if check(tmp_res): #맞으면
-                res.append([x,y,a])
-            else:
-                continue
-        #삭제 
-        else:
-            del tmp_res[tmp_res.index([x,y,a])]
-            if check(tmp_res):
-                del res[res.index([x,y,a])]
-            else:
-                continue 
-            
-    res = sorted(res, key = lambda x: (x[0], x[1], x[2]))
-    return res
-# -------------------------------------------------
-print(solution(n, build_frame))
+'''
+
+from itertools import combinations
+
+#input-----------------------
+N, M = map(int, input().split())
+mat = [ list(map(int, input().split())) for _ in range(N)]
+#---------------------------
+home_lst = []
+store_lst = []
+for i in range(len(mat)):
+    for j in range(len(mat)):
+        if mat[i][j] == 1:
+            home_lst.append((i,j))
+        elif mat[i][j] == 2:
+            store_lst.append((i,j))
+
+print(home_lst)
+print(store_lst)
+
+comb_lst = list(combinations(store_lst,M))
+city_dist_lst = []
+for comb_store_lst in comb_lst:
+    city_dist = 0
+    for home in home_lst:
+        hx, hy = home[0], home[1]
+        home_dist = 200000
+        for store in comb_store_lst:
+            sx, sy = store[0], store[1]
+            new = abs(hx - sx) + abs(hy - sy)
+            # print('new', new)
+            home_dist = min(home_dist, new)
+            # print(home_dist)
+        #여기서 집1의 가장 가까운 home_dist가 나오고
+        #이걸 city_dist에 업데이트해줘야해 
+        city_dist += home_dist
+    city_dist_lst.append(city_dist)
+        
+print(min(city_dist_lst))
+
