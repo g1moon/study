f16c50e73466744337066dbbbd54fb2c63604c57
g1moon <gmoon.cho@gmail.com>
2020-11-10 17:43:23 +0900

    [upload]

--- Algorithms-PS/inflearn-algorithm/src/sec6_10_조합구하기_2.py
+++ Algorithms-PS/inflearn-algorithm/src/sec6_10_조합구하기_2.py
@@ -4,7 +4,8 @@
 
 def dfs(idx, start):
     if idx == M:
-        print(res)
+#        print(res)
+        res_lst.append(res) #이부분...
         return 
     
     else:
@@ -14,6 +15,7 @@ def dfs(idx, start):
         
 
 if __name__ == "__main__":
+    res_lst = [] #종료조건에서의 res를 리스트에 append해 관리하고 싶습니다.
     N,M = map(int, input().split())
     cnt = 0 
     res = [ 0 for i in range(M)]
