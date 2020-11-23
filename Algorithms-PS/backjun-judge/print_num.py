807dde7020706eda8f4d28c919415c8a7faacd77
g1moon <gmoon.cho@gmail.com>
2020-11-23 17:43:19 +0900

    [upload]

--- Algorithms-PS/backjun-judge/print_num.py
+++ Algorithms-PS/backjun-judge/print_num.py
@@ -1,19 +1,10 @@
-#자연수 n입력
-#첫째행에는 1개원소
-#둘째행에는 2배씩 출력되는 원소
-n = int(input('N = '))
-r = 0
-num = 0 
-op = True
-while op == True:
-    for i in range(2**r):
-        num += 1
-        print(num, end=' ')
-        if num == n:
-            op = False
-            break
-    
+#boj 9095 
+#Adding 1s, 2s, and 3s
 
-    r += 1
-    print('\n', end ='')
-    
+import sys 
+input = sys.stdin.readline()
+
+t = int(input)
+for _ in range(t):
+    n = int(input)
+    print(n)
