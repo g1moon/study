c03a0974937bc7c6632c18042615581d680aebf1
g1moon <gmoon.cho@gmail.com>
2020-12-03 19:47:45 +0900

    [upload]

--- Web-Programming/JavaScript/main.js
+++ Web-Programming/JavaScript/main.js
@@ -1,1 +1,41 @@
-console.log('hello world!');
+'use strict'
+
+class Counter{
+    constructor(runEvryFiveTime){
+        this.counter = 0;
+        this.callback = runEvryFiveTime;
+    }
+
+    increase() {
+        this.counter++;
+        console.log(this.counter);
+        if (this.counter % 5 === 0){
+            // if (this.callback){
+            //     this.callback(this.counter);
+            // } --> 간단하게
+            this.callback && this.callback(this.counter); //simple ver
+        }
+    }
+}
+
+//----------------------------------
+function printSomething(num){
+    console.log(`wow! ${num}`);
+}
+
+function alertNum(num){
+    alert(`alert! ${num}`);
+}
+
+const coolCounter = new Counter(printSomething);
+const alertCounter = new Counter(alertNum);
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
