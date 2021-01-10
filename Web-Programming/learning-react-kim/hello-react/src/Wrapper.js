ddbc18be8ccd07ab5fd788d9bc68a7ff4379e6b6
g1moon <gmoon.cho@gmail.com>
2021-01-10 15:54:32 +0900

    Add example for studying children

--- Web-Programming/learning-react-kim/hello-react/src/Wrapper.js
+++ Web-Programming/learning-react-kim/hello-react/src/Wrapper.js
@@ -17,17 +17,30 @@ export default Wrapper;
 
 //App.js
 // //-----------------------------------------------
+// // import React from 'react';
+// // import Hello from './Hello';
+// // import Wrapper from './Wrapper';
+// //
+// // function App() {
+// //     return (
+// //         <Wrapper>
+// //             <Hello color="red" name="react" />
+// //             <Hello color="pink"/>
+// //         </Wrapper>
+// //     );
+// // }
+// //
+// // export default App;
+
+//Hello.js
 // import React from 'react';
-// import Hello from './Hello';
-// import Wrapper from './Wrapper';
 //
-// function App() {
-//     return (
-//         <Wrapper>
-//             <Hello color="red" name="react" />
-//             <Hello color="pink"/>
-//         </Wrapper>
-//     );
+// function Hello({color, name}) {
+//     return <div style = {{color}}> 안녕하세요 {name}</div>
 // }
 //
-// export default App;
+// Hello.defaultProps = {
+//     name: '이름없음'
+// }
+//
+// export default Hello;
