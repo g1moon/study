ea69295a090458459e18e32483639dc6ce029f50
g1moon <gmoon.cho@gmail.com>
2021-01-22 20:54:47 +0900

    Add example for studying React Router

--- Web-Programming/learning-react-kim/router-tutorial/src/App.js
+++ Web-Programming/learning-react-kim/router-tutorial/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+// import Profile from './Profile';
+import Profiles from "./Profiles";
+
 const App = () => {
     return (
         <>
         <div>
             <Route path = '/' component={Home} exact={true} />
             <Route path ={['/about', '/info']} component={About} />
-            <Route path = '/profile/:username' component={Profile}/>
+            {/*<Route path = '/profile/:username' component={Profile}/>*/}
+            <Route path = '/profiles' componet = {Profiles}/>
         </div>
         </>
 
