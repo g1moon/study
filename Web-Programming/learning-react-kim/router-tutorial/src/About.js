51207f8efbc9193b1092032aabab6b595066c21c
g1moon <gmoon.cho@gmail.com>
2021-01-22 18:39:12 +0900

    Add example for studying immutablility with immer

---
+++ Web-Programming/learning-react-kim/router-tutorial/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div>
+            <h1>소개</h1>
+            <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 페이지(소개페이지)</p>
+        </div>
+    )
+};
+
+export default About;
---
+++ Web-Programming/learning-react-kim/router-tutorial/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>홈</h1>
+            <p>홈, 이 페이지는 가장 먼저 포여지는 페이지 입니당</p>
+        </div>
+    )
+};
+
+export default Home;
